cd8bf3f7b8baa94cf991d6a88df39015971446bf
aashalabi <ahmed.shalabi@gmail.com>
2025-02-02 21:36:42 -0800

    Update training script

--- src/train.py
+++ src/train.py
@@ -19,15 +19,19 @@ data, meta = arff.loadarff(r'Training Dataset.arff')
 # Convert to Pandas DataFrame
 df = pd.DataFrame(data)
 
-# Decode all byte-string columns into int
+# Decode all byte-string columns into strings
 for column in df.columns:
     if df[column].dtype == 'object':  # Byte strings are usually stored as 'object' dtype
         try:
-            df[column] = df[column].str.decode('utf-8').astype(int)  # Decode to regular strings
+            df[column] = df[column].str.decode('utf-8')  # Decode to regular strings
         except AttributeError:
             pass  # Column doesn't contain byte strings
 
 df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+#Change the result as type int
+df['result'] = df['result'].astype(int)
+
 
 df_full_train, df_test = train_test_split(df, test_size = 0.2, random_state = 11)
 df_train, df_val = train_test_split(df_full_train, test_size = 0.25, random_state = 11)
@@ -46,16 +50,19 @@ del df_train['result']
 del df_val['result']
 del df_test['result']
 
-X_train = df_train
-X_val = df_val
-X_test = df_test
+
+categorical = list(df_train.dtypes[df.dtypes == 'object'].index)
+numerical = list(df_train.dtypes[df.dtypes != 'object'].index)
+
+#create train dict for hot encoding
+train_dict = df_train[categorical + numerical].to_dict(orient='records')
+
 
 
 dv = DictVectorizer(sparse=False)
-X_train_dic = X_train.to_dict(orient='records')
-X_train_v = dv.fit_transform(X_train_dic)
+X_train = dv.fit_transform(train_dict)
 rf = RandomForestClassifier(n_estimators=25,
-                                    max_depth=10,
+                                    max_depth=15,
                                     random_state=1)
 
 rf.fit(X_train, y_train)
